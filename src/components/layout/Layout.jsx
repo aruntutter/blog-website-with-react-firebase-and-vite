196b07a83c6fb1c1b74907359a1d5972314b7425
Arun Kumar R <aruntutter19@gmail.com>
2023-10-17 21:46:18 +0530

    layout added to home page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="content min-h-screen">{children}</div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
