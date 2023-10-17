4e212f077fa42396040eaf4fba48b1fa1cb43aa0
Arun Kumar R <aruntutter19@gmail.com>
2023-10-17 18:11:54 +0530

    components and folders created

---
+++ src/components/shareDialog/ShareDialog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShareDialog = () => {
+  return <div>ShareDialog</div>;
+};
+
+export default ShareDialog;
---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirebaseConfig = () => {
+  return <div>FirebaseConfig</div>;
+};
+
+export default FirebaseConfig;
